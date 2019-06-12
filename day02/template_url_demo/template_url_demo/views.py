8c338bbf72392eba8899322c23c1d39141ae8d6a
gaohj <gaohj@126.com>
2019-06-12 17:38:26 +0800

    day0202_if url

---
+++ day02/template_url_demo/template_url_demo/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,'index.html')
+
+def login(request):
+    next = request.GET.get('next')
+    text = "登录界面，登录完成后要跳转的url是:%s" % next
+    return HttpResponse(text)
+
+def book(request):
+    return HttpResponse("读书页面")
+
+def book_detail(request,book_id,category):
+    text = "图书id是:%s,分类是:%s" % (book_id,category)
+    return HttpResponse(text)
+def movie(request):
+    return HttpResponse("电影页面")
+
+def city(request):
+    return HttpResponse("同城页面")
