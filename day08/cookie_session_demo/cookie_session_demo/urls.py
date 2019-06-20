30e6c6c5ed52c944e32d9aabd3f9b4d94ffc0ebd
gaohj <gaohj@126.com>
2019-06-20 17:46:28 +0800

    day8 cookie

---
+++ day08/cookie_session_demo/cookie_session_demo/urls.py
@@ -0,0 +1,26 @@
+"""cookie_session_demo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('delete/',views.delete_cookie,name='delete'),
+    path('cms/',views.cms_view,name='cms'),
+    path('my/',views.my_view,name='my'),
+    path('session/',views.session_view,name='session'),
+]
---
+++ day08/cookie_session_demo/cookie_session_demo/views.py
@@ -0,0 +1,39 @@
+from django.http import HttpResponse
+from datetime import datetime
+from django.utils.timezone import make_aware
+
+
+def index(request):
+    response = HttpResponse("Index")
+    expires = datetime(year=2019,month=6,day=22,hour=20,minute=30,second=40)
+    expires = make_aware(expires)
+    response.set_cookie('user_id','xcxk',expires=expires,max_age=180,path='/cms/')
+    return response
+
+def delete_cookie(request):
+    response = HttpResponse('delete')
+    response.delete_cookie('user_id')
+    return response
+
+def cms_view(request):
+    cookies = request.COOKIES
+    username = cookies.get('user_id')
+    return HttpResponse(username)
+
+def my_view(request):
+    cookies = request.COOKIES
+    username = cookies.get('user_id')
+    return HttpResponse(username)
+
+def session_view(request):
+    request.session['username'] = 'xxcxk'
+    request.session['user_id'] = '444'
+    # username = request.session.pop('username') #从session移除指定的字段
+    # print(username) #打印的是移除的哪个值
+    # request.session.clear()
+    # #request.session.flush() #注销登录常用这个
+    # print(request.session.get('username')) #查看移除以后session中的指定的值
+    # request.session.set_expiry(None) #表示两周
+    request.session.set_expiry(-1) #表示永不过期
+    request.session.clear_expired() #清空过期的session
+    return HttpResponse("SESSION")
